0a39183ebe6d92b31bc24d77b7010db9a899fc40
smitavqa <smitav.qa@gmail.com>
2026-02-07 16:44:56 +0530

    commit1

---
+++ src/main/java/smita/A.java
@@ -0,0 +1,7 @@
+package smita;
+
+public class A {
+public static void main(String[] args) {
+	System.out.println("hi");
+}
+}
---
+++ src/main/java/smita/B.java
@@ -0,0 +1,10 @@
+package smita;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("Hi B");
+
+	}
+
+}
